80807aed7e584bb56370799bbe6aeb45f80d81fe
Ankit Sharma <sharmaji20202006@gmail.com>
2026-01-22 00:44:36 +0530

    Find longest subarray with equal even and odd elements

---
+++ longest_even_odd_subarray.c
@@ -0,0 +1,40 @@
+//Q. Find the longest contiguous subarray with equal number of even and odd element.
+
+
+#include <stdio.h>
+
+int main(){
+    int arr[100], n;
+
+    printf("Enter the length of the array: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    int max_len = 0;
+    
+    for(int i = 0; i < n; i++){
+        int even_count=0;
+        int odd_count=0;
+        for(int j = i; j < n; j++){
+            if(arr[j]%2==0){
+                even_count++;
+            }else{
+                odd_count++;
+            }
+            if(even_count == odd_count){
+                int length=j-i+1;
+                if(length> max_len){
+                    max_len = length;
+                }
+            }
+
+        }
+    }
+
+    printf("longest contiguous subarray with equal number of even and odd element = %d\n", max_len);
+    return 0;
+}
