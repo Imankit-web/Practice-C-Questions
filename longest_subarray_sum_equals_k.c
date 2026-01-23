b2f7389240f2dfcd4501d8ded0119150ef3dc701
Ankit Sharma <sharmaji20202006@gmail.com>
2026-01-24 00:27:09 +0530

    Find longest subarray with sum equal to K

---
+++ longest_subarray_sum_equals_k.c
@@ -0,0 +1,40 @@
+//Q. Find the longest subarray with sum exactly equal to K.
+
+
+#include <stdio.h>
+
+int main(){
+    int arr[100], n, k;
+
+    printf("Enter the length of the array: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the value of K: ");
+    scanf("%d", &k);
+
+    int max_len=0;
+
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+
+        for(int j = i; j < n; j++){
+            sum += arr[j];
+            if(sum==k){
+                int length=j-i+1;
+                if(max_len<length){
+                    max_len=length;
+                }
+            }
+            
+        }
+    }
+
+    printf("%d",max_len);
+    return 0;
+}
+
