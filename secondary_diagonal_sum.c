32ff6ad926133da0543042cfaa06ffb55f956590
Ankit Sharma <sharmaji20202006@gmail.com>
2026-02-10 20:05:33 +0530

    Secondary diagonal sum of square matrix

---
+++ secondary_diagonal_sum.c
@@ -0,0 +1,33 @@
+// Q. Find the Sum of the Secondary Diagonal of a Square Matrix
+
+#include <stdio.h>
+
+int main() {
+    int arr[100][100], n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Input matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("Enter the element: ");
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int sum = 0;
+
+    // Secondary diagonal sum
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i + j == n - 1) {
+                sum += arr[i][j];
+            }
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
