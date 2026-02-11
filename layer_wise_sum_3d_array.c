563391ab3d1551fb9232b7a59a721dab0b23b1b0
Ankit Sharma <sharmaji20202006@gmail.com>
2026-02-12 01:05:58 +0530

    Layer-wise sum of a 3D array

---
+++ layer_wise_sum_3d_array.c
@@ -0,0 +1,44 @@
+//Q. Find the Sum of Elements in Each Layer of a 3D Array
+
+
+#include <stdio.h>
+
+int main(){
+    int arr[10][10][10];
+    int l, r, c;
+
+    printf("Enter number of layers: ");
+    scanf("%d", &l);
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    
+    // Input 3D array
+    for (int layer = 0; layer < l; layer++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                printf("Enter element: ");
+                scanf("%d", &arr[layer][i][j]);
+            }
+        }
+    }
+
+    for (int layer = 0; layer < l; layer++) {
+        int sum =0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                sum+= arr[layer][i][j];
+            }
+           
+        }
+        printf("%d \n",sum);
+    }
+
+
+    return 0;
+
+}
