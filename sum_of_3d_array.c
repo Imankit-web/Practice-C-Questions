741b92b63fac521fd211fc626928ba9ce309bad1
Ankit Sharma <sharmaji20202006@gmail.com>
2026-02-12 00:58:21 +0530

    Sum of all elements in a 3D array

---
+++ sum_of_3d_array.c
@@ -0,0 +1,43 @@
+//Q. Find the Sum of All Elements in a 3D Array
+
+#include <stdio.h>
+
+int main(){
+    int arr[10][10][10];
+    int l, r, c;
+
+    printf("Enter number of layers: ");
+    scanf("%d", &l);
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    
+    // Input 3D array
+    for (int layer = 0; layer < l; layer++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                printf("Enter element: ");
+                scanf("%d", &arr[layer][i][j]);
+            }
+        }
+    }
+
+    int sum =0;
+    for (int layer = 0; layer < l; layer++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                sum+= arr[layer][i][j];
+            }
+           
+        }
+    }
+
+    printf("%d",sum);
+
+    return 0;
+
+}
