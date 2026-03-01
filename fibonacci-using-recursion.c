42fa1805b74c99e00f9d39a3a0d4bb8698db5ed4
Ankit Sharma <sharmaji20202006@gmail.com>
2026-03-01 11:47:16 +0530

    Print Fibonnacci series using recursion

---
+++ fibonacci-using-recursion.c
@@ -0,0 +1,18 @@
+//Q. Wap to print the Fibonacci series using recursion.
+
+#include <stdio.h>
+
+void fib(int n , int a, int b){
+    if(n == 0){
+        return;
+    }
+    printf("%d",a);
+    fib(n-1,b,a+b);
+}
+int main(){
+    int n;
+    printf("Enter the range:");
+    scanf("%d",&n);
+    fib(n,0,1);
+    return 0;
+}
